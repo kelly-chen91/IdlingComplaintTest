691e72978ad1a211478aa89a3b6fc62245b1fd36
Kelly Chen <kchen@dep.nyc.gov>
2023-06-15 12:00:42 -0400

    Added a cancel button test

--- IdlingComplaintTest/Tests/CreateAnAccountTests/RegistrationSuccessTest.cs
+++ IdlingComplaintTest/Tests/CreateAnAccountTests/RegistrationSuccessTest.cs
@@ -44,5 +44,29 @@ namespace IdlingComplaintTest.Tests.CreateAnAccountTests
             Assert.IsNotNull(loginButton);
             Thread.Sleep(60000);
         }
+
+        [Test]
+        [Category("Cancel Registration Test")]
+        public void ImplicitWaitCancelRegistrationTest()
+        {
+            createAnAccountModel.EnterFirstName("Jane");
+            createAnAccountModel.EnterLastName("Doe");
+            createAnAccountModel.EnterEmail("kellychen966@com");
+            createAnAccountModel.EnterPassword("T3sting@1234");
+            createAnAccountModel.EnterConfirmPassword("T3sting@1234");
+            createAnAccountModel.SelectSecurityQuestion(0);
+            createAnAccountModel.EnterSecurityAnswer("Testing");
+            createAnAccountModel.EnterAddress1("59-17 Junction Blvd");
+            createAnAccountModel.EnterAddress2("10th Fl");
+            createAnAccountModel.EnterCity("Queens");
+            createAnAccountModel.SelectState(0);
+            createAnAccountModel.EnterZipCode("11373");
+            createAnAccountModel.EnterPhone("631-632-9800");
+            createAnAccountModel.ScrollToButton();
+            createAnAccountModel.ClickCancelButton();
+            var loginButton = GetDriver().FindElement(By.XPath("/html/body/app-root/div/app-login/mat-card/mat-card-content/form/div[3]/button"));
+            Assert.IsNotNull(loginButton);
+            //Thread.Sleep(60000);
+        }
     }
 }
